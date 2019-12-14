e8db77d46041eee2950877c09ac07a3298275321
zyh888888 <97571035@qq.com>
2019-12-14 23:59:49 +0800

    'post-index/show(bindModel)/create(get)/store(post)'

---
+++ app/Http/Tool/Tool.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace App\Http\Tool;
+
+
+class Tool
+{
+    public function SuccessWithDate()
+    {
+
+    }
+}
--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -3,22 +3,21 @@
         <div class="col-sm-8 blog-main">
             <div class="blog-post">
                 <div style="display:inline-flex">
-                    <h2 class="blog-post-title">{{$title}}你好你好</h2>
-                    @if($isShow ===true )
-                    <a style="margin: auto"  href="/posts/62/edit">
+                    <h2 class="blog-post-title">{{$post->title}}</h2>
+                    <a style="margin: auto"  href="/posts/{{$post->id}}/edit">
                         <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
                     </a>
-                    <a style="margin: auto"  href="/posts/62/delete">
+                    <a style="margin: auto"  href="/posts/{{$post->id}}/delete">
                         <span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
                     </a>
-                    @endif
                 </div>
 
-                <p class="blog-post-meta">May 14, 2017 by <a href="#">Kassandra Ankunding2</a></p>
+                <p class="blog-post-meta">{{$post->created_at}}by <a href="#">Kassandra Ankunding2</a></p>
 
-                <p><p>你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好<img src="http://127.0.0.1:8000/storage/72c76b674ec8793fcfd6555ff371bfbd/nxC9ozLfkORmoY92q9lPsejXchVvdNO2cwHiR2Jf.jpeg" alt="63" style="max-width: 100%;">你好你好似懂非懂说</p><p><br></p></p>
+                <p>{{$post->content}}</p>
+                <img src="http://127.0.0.1:8000/storage/72c76b674ec8793fcfd6555ff371bfbd/nxC9ozLfkORmoY92q9lPsejXchVvdNO2cwHiR2Jf.jpeg" alt="63" style="max-width: 100%;">你好你好似懂非懂说</p><p><br></p></p>
                 <div>
-                    <a href="/posts/62/zan" type="button" class="btn btn-primary btn-lg">赞</a>
+                    <a href="/posts/{{$post->id}}/zan" type="button" class="btn btn-primary btn-lg">赞</a>
 
                 </div>
             </div>
