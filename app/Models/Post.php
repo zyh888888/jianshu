f37c8f4dc571c747e9c3256f364e1539ac77edf1
zyh888888 <97571035@qq.com>
2019-12-22 17:55:24 +0800

    '1222'

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,5 +5,16 @@ namespace App\Models;
 class Post extends Model
 {
     protected $table = 'posts';
-    protected $fillable = ['uid','title','content'];
+    protected $fillable = ['user_id','title','content'];
+
+    /**
+     * @NOTES:反向关联用户表
+     * @AUTH:zhou.yh
+     * @Date:2019/12/22 13:03
+     * @Version
+     */
+    public function user()
+    {
+        return $this->belongsTo('App\User','user_id','id');
+    }
 }
